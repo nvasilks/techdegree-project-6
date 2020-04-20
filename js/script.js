ac63832464b48006d6794b06b5a60a0e3482cf54
nvasilks <61193591+nvasilks@users.noreply.github.com>
2020-04-20 05:32:49 +0100

    Add new files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const start = document.querySelector('.btn__reset');
+const overlay = document.querySelector('#overlay');
+let missed = 0;
+
+start.addEventListener('click', () => overlay.style.display = 'none');
+
+const phrases = ['Only the educated are free', 'Be the reason someone smiles', 'Everything is in flux', 'Focus on the good', 'Slow down'];
+
+
+})
